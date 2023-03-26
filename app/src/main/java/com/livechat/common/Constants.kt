bb2db816c8104cddac09cd58efa2da175713f9d5
Phúc <quangphuc2409@gmail.com>
2023-03-26 23:27:37 +0700

    Add create user

---
+++ app/src/main/java/com/livechat/common/Constants.kt
@@ -0,0 +1,13 @@
+package com.livechat.common
+
+/**
+ * User: Quang Phúc
+ * Date: 2023-03-26
+ * Time: 09:04 PM
+ */
+object Constants {
+
+    object Collections {
+        val users = "users"
+    }
+}
--- app/src/main/java/com/livechat/repo/AuthRepo.kt
+++ app/src/main/java/com/livechat/repo/AuthRepo.kt
@@ -1,6 +1,9 @@
 package com.livechat.repo
 
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FieldValue
+import com.google.firebase.firestore.FirebaseFirestore
+import com.livechat.common.Constants
 import javax.inject.Inject
 
 /**
@@ -8,7 +11,10 @@ import javax.inject.Inject
  * Date: 2023-03-22
  * Time: 09:54 PM
  */
-class AuthRepo @Inject constructor(private val firebaseAuth: FirebaseAuth) {
+class AuthRepo @Inject constructor(
+    private val firebaseAuth: FirebaseAuth,
+    private val firebaseFirestore: FirebaseFirestore
+) {
 
     fun login(
         email: String,
@@ -37,12 +43,36 @@ class AuthRepo @Inject constructor(private val firebaseAuth: FirebaseAuth) {
         firebaseAuth.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener { task ->
                 if (task.isSuccessful) {
-                    onSuccess()
+                    createUser(onSuccess, onError)
                 } else {
                     val e = task.exception as Exception
                     e.printStackTrace()
                     onError(e)
                 }
+            }
+    }
+
+    private fun createUser(onSuccess: () -> Unit, onError: (e: Exception) -> Unit) {
+        val model = hashMapOf(
+            "email" to firebaseAuth.currentUser?.email,
+            "fullName" to "",
+            "avatarUrl" to "",
+            "birthday" to FieldValue.serverTimestamp(),
+            "tokens" to ArrayList<String>(),
+            "friends" to ArrayList<String>(),
+            "createdAt" to FieldValue.serverTimestamp()
+        )
+
+        firebaseFirestore.collection(Constants.Collections.users)
+            .document(firebaseAuth.currentUser?.uid.toString())
+            .set(model)
+            .addOnSuccessListener {
+                firebaseAuth.signOut()
+                onSuccess()
+            }
+            .addOnFailureListener {
+                firebaseAuth.signOut()
+                onError(it)
             }
     }
 }
